1ec585c84b1954c8735d0464f70ac6a8842fbc4d
Alberto Lopez <albertolopez19@gmail.com>
2023-04-10 15:23:27 -0400

    Update function for adding and removing items from lanes

--- pages/components/index.tsx
+++ pages/components/index.tsx
@@ -1,54 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
-type ItemProps = {
-  message: string;
-};
+import styles from './index.module.css';
 
 function removeItem(itemIndex: number, items: LaneItem[]) {
-  return items.splice(itemIndex, 0);
+  const newItems = items;
+  newItems.splice(itemIndex, 1);
+  return newItems;
 }
 
 function addItem(itemIndex: number, items: LaneItem[], item: LaneItem) {
-  return items.splice(itemIndex, 0, item);
+  const newItems = items;
+  newItems.splice(itemIndex, 0, item);
+  return newItems;
 }
-
-const Item = ({ message }: ItemProps) => {
-  const incrementLane = () => {
-
-  };
-
-  return (
-    <li>
-      <span>
-        {message}
-        <button
-          onClick={incrementLane}
-        >
-          +
-        </button>
-      </span>
-    </li>
-  );
-};
 
 type LaneItem = {
   message: string;
 };
 
-type LaneProps = {
-  items: LaneItem[];
+enum POSITION {
+  DECREMENT,
+  INCREMENT
 };
 
-const Lane = ({ items }: LaneProps) => {
+const Lane = ({ items, moveItemToLane, name, canDecrementItem, canIncrementItem }: LaneProps) => {
   return (
-    <div>
+    <div className={styles.lane}>
+      <h1>{name}</h1>
       <ul>
         {items.map((item, index) => (
           <li key={index}>
+            <button
+              disabled={!canDecrementItem}
+              onClick={() => moveItemToLane(item, index, POSITION.DECREMENT)}
+            >
+              -
+            </button>
             <span>
               {item.message}
               <button
-                onClick={e => {}}
+                disabled={!canIncrementItem}
+                onClick={() => moveItemToLane(item, index, POSITION.INCREMENT)}
               >
                 +
               </button>
@@ -69,14 +61,18 @@ const mockLanes = [
     name: "Bob",
     items: [
       mockItem,
+      { message: "test1" },
+      { message: "test2" },
     ]
   },
   {
     name: "George",
     items: [
       mockItem,
+      { message: "test3" },
+      { message: "test4" },
     ]
-  }
+  },
 ];
 
 type Lane = {
@@ -84,28 +80,81 @@ type Lane = {
   items: LaneItem[];
 }
 
+type LaneProps = {
+  name: string;
+  items: LaneItem[];
+  moveItemToLane: Function;
+  canIncrementItem: boolean;
+  canDecrementItem: boolean;
+};
 
 export default function TorcPage() {
   const [lanes, setLanes] = useState<Lane[]>(mockLanes);
 
-  const removeItemFromLane = (index: number) => {
-    const newItems = removeItem(index, items);
-    setItems(newItems);
+  const removeItemFromLane = (itemIndex: number, lane: Lane) => {
+    const newItems = removeItem(itemIndex, lane.items);
+    return { ...lane, items: newItems };
   };
 
-  const addItemToLane = (index: number, item: LaneItem) => {
-    if (lane[]) {
-
-    }
-    const newItems = addItem(index, items, item);
-    setItems(newItems);
+  const addItemToLane = (itemIndex: number, laneIndex: number, item: LaneItem) => {
+    const newItems = addItem(itemIndex, lanes[laneIndex].items, item);
+    return { ...lanes[laneIndex], items: newItems };
   };
+
+  const moveItemToLane = (
+    itemIndex: number,
+    newLaneIndex: number,
+    originalLaneIndex: number,
+    item: LaneItem
+  ) => {
+    const newLane = addItemToLane(itemIndex, newLaneIndex, item);
+    const oldLane = removeItemFromLane(itemIndex, lanes[originalLaneIndex]);
+
+    setLanes((lanes) => {
+      const newLanes = lanes.map((lane, index) => {
+        if (index === originalLaneIndex) {
+          return oldLane;
+        } else if (index === newLaneIndex) {
+          return newLane;
+        }
+
+        return lane;
+      });
+
+      return newLanes;
+    });
+  };
+
+  const canIncrementItem = useCallback((index: number) => {
+    return lanes[index + 1] !== undefined;
+  }, [lanes]);
+
+  const canDecrementItem = useCallback((index: number) => {
+    return lanes[index - 1] !== undefined;
+  }, [lanes]);
 
   return (
-    <div>
-      {lanes.map((lane, index)) => (
-        <Lane key={index} lane={lane} addItemToLane={addItem} />
-      )}
+    <div className={styles.grid}>
+      {lanes.map((lane, index) => {
+        return (
+          <React.Fragment key={index}>
+            <Lane
+              name={lane.name}
+              items={lane.items}
+              key={index}
+              moveItemToLane={(item: LaneItem, itemIndex: number, position: POSITION) => {
+                if (position === POSITION.INCREMENT) {
+                  moveItemToLane(itemIndex, index + 1, index, item);
+                } else {
+                  moveItemToLane(itemIndex, index - 1, index, item);
+                }
+              }}
+              canIncrementItem={canIncrementItem(index)}
+              canDecrementItem={canDecrementItem(index)}
+            />
+          </React.Fragment>
+        );
+      })}
     </div>
   );
 }
